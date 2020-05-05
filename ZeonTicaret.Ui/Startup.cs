ba806f6fbeb4c54d00e99d4ce6f45145f0c0040f
muratcan döngel <muratcan_dongel@hotmail.com>
2020-05-05 14:54:36 +0300

    Add project files.

---
+++ ZeonTicaret.Ui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZeonTicaret.Ui.Startup))]
+namespace ZeonTicaret.Ui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
